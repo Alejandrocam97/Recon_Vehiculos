998fbf46285b82730443f174e80fdc2818f6999a
Alejandro Cambronero <cambro97@gmail.com>
2021-04-08 01:11:32 +0200

    pruebas de erosion y dilatacion

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/photo/cuda.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
+using namespace cuda;
 
 void poligon(vector<vector<Point>> forma);
 
 
 int main()
 {
-	Mat im, gray, gauss, bin, erosion, dilatacion;
+	Mat im, gray, gauss, bin, erosion, dilatacion, noise;
+	
 	Mat prueba[22];
 	vector<vector<Point>> Contours;
 
@@ -27,22 +30,36 @@ int main()
 
 	imshow("original", im);
 	cvtColor(im, gray, COLOR_BGR2GRAY);
-	GaussianBlur(gray, gauss, Size(5, 5), 0, 0);
-	//erode(gauss, erosion, Mat(), Point(-1, -1), 3);
+	GaussianBlur(gray, gauss, Size(9, 9), 0, 0);
+	//fastNlMeansDenoisingColored(im, noise, float(20), float(10));
+
+	//imshow("gaus", noise);
+	//waitKey(0);
+	//erode(gauss, erosion, getStructuringElement(MORPH_RECT, Size(5, 5)), Point(-1, -1), 3);
+
 	//dilate(erosion, dilatacion, Mat(), Point(-1, -1), 1);
 
 	//imshow("gauss", gauss);
-	//imshow("erosion", dilatacion);
+	//imshow("erosion", erosion);
 	//waitKey(0);
-	/*for (int i = 0; i < 22; i++)
+	for (int i = 0; i < 22; i++)
 	{
-		threshold(gauss, prueba[i], (20+i*10), 255, THRESH_BINARY);
-		imshow("prueba", prueba[i]);
+		int val = 20 + i * 10;
+		threshold(erosion, prueba[i], val , 255, THRESH_BINARY);
+		//imshow("prueba", prueba[i]);
+  		//waitKey(0);
+		findContours(prueba[i], Contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+		Mat drawing = Mat::zeros(im.size(), CV_8UC3);
+		drawContours(drawing, Contours, -1, Scalar(255, 0, 0), 2);
+
+
+		imshow("Contornos", drawing);
 		waitKey(0);
 
-	}*/
+	}
 	//threshold(gauss, bin, 120, 255, THRESH_BINARY);
-	threshold(gauss, bin, 115, 255, THRESH_BINARY);
+	//threshold(erosion, bin, 115, 255, THRESH_BINARY);
 
 
 	findContours(bin, Contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
